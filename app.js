1c0d5a95637c898fc02153890dfcae26c91c71a1
Vinecreeper888 <siddhanth8@gmail.com>
2020-10-07 10:33:59 +0530

    basic authentication

--- app.js
+++ app.js
@@ -40,60 +40,38 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 const auth = (req,res,next) => {
   console.log(req.session);
 
   if(!req.session.user) {
     //authenticate self
-    var authHeader = req.headers.authorization;
-
-    if(!authHeader) {
-      var err = new Error('Yu are not authenticated!');
-      res.setHeader('www-Authenticate','Basic');
+    // var authHeader = req.headers.authorization;
+      var err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err); //goto the error handler
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-
-    if(username === 'admin' && password === 'password') {
-      req.session.user = 'admin';
-      next(); //to proceed forward
-    }
-    else {
-      //error
-      var err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-      return next(err);
-    }
   } else {
-    if(req.session.user === 'admin') {
+    if(req.session.user === 'authenticated') {
       //contains correct info
       next();
     } else {
       //this cookie is not valid
       var err = new Error("You are not authenticated!");
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
 
 }
 
-
-
-
 app.use(auth);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promos',promoRouter);
 app.use('/leaders',leaderRouter);
